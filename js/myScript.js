ead4689d3c9edcf1af4698004c9597c7cf4545d9
luk93 <ligajlukasz93@gmail.com>
2025-12-11 23:46:30 -0600

    projects added

--- js/myScript.js
+++ js/myScript.js
@@ -6,6 +6,9 @@ var img1 = document.getElementById("project_img1");
 var img2 = document.getElementById("project_img2");
 var img3 = document.getElementById("project_img3");
 var img4 = document.getElementById("project_img4");
+var img5 = document.getElementById("project_img5");
+var img6 = document.getElementById("project_img6");
+var img7 = document.getElementById("project_img7");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img1.onclick = function(){
@@ -28,7 +31,21 @@ img4.onclick = function(){
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
-
+img5.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+img6.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+img7.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
